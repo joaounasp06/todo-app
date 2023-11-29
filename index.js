731c9c990bbc268cb52c9116ca93e02ef12fdb0d
Nelson <joaonelson08@gmail.com>
2023-11-29 09:33:16 -0300

    salvando uma tarefa no banco de dados

--- index.js
+++ index.js
@@ -1,23 +1,55 @@
 const express = require("express")
-const exphbs = require("express-handlebars")
+const app = express()
 const mysql = require("mysql2")
 
-const app = express()
+app.get("/", (req,res)=>{
+    res.render('home')
+})
 
+
+
+// --Configurando o Handlebars 
+// -Para baixar o Handlebars: npm i express-handlebars
+const exphbs=require("express-handlebars")
 app.engine('handlebars', exphbs.engine())
 app.set('view engine', 'handlebars')
 
+// --Definindo uma pasta de arquivos estáticos 
 app.use(express.static('public'))
 
-app.use (express.urlencoded({
+// criando uma conexão com o MYSQL:
+const conexao = mysql.createConnection({
+    host: "localhost",
+    user:"root",
+    password:"root",
+    database:"todo.app",
+    // Caso o banco de dados esteja rodando na porta 3307 é necessario mudar a port a baixo para port: 3307
+    port: 3306
+})
+
+conexao.connect((erro)=>{
+    if (erro){
+        return console.log(erro)
+    } 
+    
+    console.log("conectado ao mysql")
+
+    app.listen(3000, ()=>{
+        console.log("Servidor rodando na porta 3000")
+    })
+})
+
+// Convertendo a informação do HTML para que o js entenda
+
+app.use(express.urlencoded({
     extended: true
 }))
 
 app.use(express.json())
 
-app.post('/criar', (requisicao, reposta) => {
-    const descricao = requisicao.body.descricao
-    
+// ROTAS
+app.post("/criar", (req,res)=>{
+    const descricao= req.body.descricao 
     const completa = 0
 
     const sql = `
@@ -25,35 +57,11 @@ app.post('/criar', (requisicao, reposta) => {
         VALUES ('${descricao}', '${completa}')
     `
 
-        conexao.query(sql, (erro) => {
-           if (erro) {
+    conexao.query(sql, (erro)=>{
+        if (erro){
             return console.log(erro)
-           } 
+        }
 
-           resposta.redirect('/')
-        })
-})
-
-app.get('/', (requisicao, resposta) => {
-    resposta.render('home')
-})
-
-const conexao = mysql.createConnection ({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "todoapp",
-    port: 3306
-})
-
-conexao.connect((erro) => {
-    if (erro) {
-        return console.log(erro)
-    }
-
-    console.log("Estou conectado ao MySQL!")
-
-    app.listen(3000, () => {
-        console.log("Servidor rodando na porta 3000!")
+        res.redirect('/')
     })
 })
--- public/js/script.js
+++ public/js/script.js
@@ -1,47 +1,42 @@
-function alterarTema() {
+function alterartema(){
     const tema = localStorage.getItem("tema")
-    const body = document.querySelector("body")
+    const body= document.querySelector("body")
     const button = document.querySelector(".tema-button")
 
     if (tema) {
-        let novoTema
+        let novotema
 
-        if (tema === "light") {
-            novoTema = "dark"
-            button.innerHTML = `<img src="/imagens/sun-icon.png" alt="ícone de Sol.">`
+        if (tema === "light"){
+            novotema="dark"
+            button.innerHTML = `<img src="/imagens/sun-icon.png" alt="Icone de sol">`
             body.classList.remove("light")
             body.classList.add("dark")
-        } else {
-            novoTema = "light"
-            button.innerHTML = `<img src="/imagens/moon-icon.png" alt="ícone de Lua.">`
+        } else{
+            novotema="light"
+            button.innerHTML = `<img src="/imagens/moon-icon.png" alt="Icone de lua">`
             body.classList.remove("dark")
             body.classList.add("light")
         }
 
-        localStorage.setItem("tema", novoTema)
+        localStorage.setItem("tema", novotema)
         return
     }
 
-    localStorage.setItem("tema", "dark")
-    body.classList.add("dark")
+    localStorage.setItem("tema","dark")
 }
 
-
-
-function verificarTema() {
+function verifiicarTema(){
     const tema = localStorage.getItem("tema")
     const body = document.querySelector("body")
     const button = document.querySelector(".tema-button")
 
-    if (tema) {
-        if (tema === "dark") {
-            body.classList.add("dark")
-            button.innerHTML = `<img src="/imagens/sun-icon.png" alt="ícone do Sol.">`
-        } else {
-            body.classList.add("light")
-            button.innerHTML = `<img src="/imagens/moon-icon.png" alt="ícone de Lua.">`
-        }
+    if (tema === "dark") {
+        button.innerHTML = `<img src="/imagens/sun-icon.png" alt="Icone de sol">`
+        body.classList.add("dark")
+    }   else{
+        body.classList.add("light")
+        button.innerHTML = `<img src="/imagens/moon-icon.png" alt="Icone de lua">`
     }
 }
 
-verificarTema()
+verifiicarTema()
